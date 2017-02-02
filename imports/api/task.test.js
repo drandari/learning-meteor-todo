c2cacd839b838adcbe0536f2bb9ecd3274d260c1
Addrian Pérez <drandari@gmail.com>
2017-02-02 16:59:52 -0300

    Added Testing Unit

---
+++ imports/api/task.test.js
@@ -0,0 +1,12 @@
+/* eslint-env mocha */
+
+import { Meteor } from 'meteor/meteor';
+
+if (Meteor.isServer) {
+  describe('Task', () => {
+    describe('methods', () => {
+      it('can delete owned task', () => {        
+      });
+    });
+  });
+}
